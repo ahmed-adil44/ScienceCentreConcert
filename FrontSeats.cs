035615d1a2193c2bbb4817f74f8145af01c8a187
Ahmed Adil <Ahmed Adil@LAPTOP-C2DB8JDK>
2024-11-11 19:48:13 +0000

    Adding new project

---
+++ FrontSeats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Science_Centre_concert
+{
+    internal class FrontSeat
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Science_Centre_concert
+{
+    internal class Program
+    {
+        static void Menu()
+        {
+            Console.WriteLine("Please choose one of the options:");
+            Console.WriteLine("************************************");
+            Console.WriteLine("1) Book Seat.");
+            Console.WriteLine("2) cancel Seat.");
+            Console.WriteLine("3) Show concert info.");
+            Console.WriteLine("4) Exit.");
+            Console.WriteLine("");
+
+
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
